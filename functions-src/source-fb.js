7fdc348a260d85ad1b10c2be664b68bc80be35f7
Andrew Kirillov <kirillov.edu@gmail.com>
2020-07-24 22:58:01 -0400

    implemented basic JWT auth for Functions endpts

--- functions-src/source-fb.js
+++ functions-src/source-fb.js
@@ -4,6 +4,7 @@
 
 const moment = require("moment")
 const { v4: uuidv4 } = require("uuid")
+const jwt = require("jsonwebtoken")
 
 const gh = require("./github")
 const { FB } = require("fb")
@@ -17,66 +18,71 @@ FB.setAccessToken(process.env.FB_KEY)
  * @param {Object} context: Provided by Netlify if Identity is enabled, contains a `clientContext` object with `identity` and `user` properties.
  * @param {netlifyCallback} callback: Defined like callback in an AWS Lambda function, used to return either an error, or a response object.
  */
-const sourceFB = (_event, context, callback) => {
-  if (context.clientContext) {
-    gh.init()
-      .then(() => {
-        FB.api(
-          "",
-          "post",
-          {
-            batch: [
-              { method: "get", relative_url: "/me/events" },
-              { method: "get", relative_url: "/techvista18/events" },
-            ],
-          },
-          function(response) {
-            const filesToPush = []
+const sourceFB = (_event, _context, callback) => {
+  const token = event.headers.Authorization.replace(/Bearer/i, "").trim()
+  jwt.verify(token, process.env.JWT_SECRET, error => {
+    if (!error) {
+      gh.init()
+        .then(() => {
+          FB.api(
+            "",
+            "post",
+            {
+              batch: [
+                { method: "get", relative_url: "/me/events" },
+                { method: "get", relative_url: "/techvista18/events" },
+              ],
+            },
+            function(response) {
+              const filesToPush = []
 
-            response.forEach(item => {
-              const file = JSON.parse(item.body)
-              file.data.forEach(event => {
-                const date = moment(event.start_time).format("YYYY-MM-DD")
-                const id = uuidv4()
+              response.forEach(item => {
+                const file = JSON.parse(item.body)
+                file.data.forEach(event => {
+                  const date = moment(event.start_time).format("YYYY-MM-DD")
+                  const id = uuidv4()
 
-                filesToPush.push({
-                  content: JSON.stringify({
-                    templateKey: "fb-post",
-                    id,
-                    source: "fb",
-                    title: event.name,
-                    url: `facebook.com/${event.id}`,
-                    page: "test page",
-                    description: event.description,
-                    date,
-                    location: Object.prototype.hasOwnProperty.call(
-                      event,
-                      "place"
-                    )
-                      ? event.place.name
-                      : "",
-                  }),
-                  path: `src/data/fb/fb-${date}-${id}.json`,
+                  filesToPush.push({
+                    content: JSON.stringify({
+                      templateKey: "fb-post",
+                      id,
+                      source: "fb",
+                      title: event.name,
+                      url: `facebook.com/${event.id}`,
+                      page: "test page",
+                      description: event.description,
+                      date,
+                      location: Object.prototype.hasOwnProperty.call(
+                        event,
+                        "place"
+                      )
+                        ? event.place.name
+                        : "",
+                    }),
+                    path: `src/data/fb/fb-${date}-${id}.json`,
+                  })
                 })
               })
-            })
 
-            gh.pushFiles(
-              `FB-JSON storage in CMS on ${Date()}`,
-              filesToPush
-            ).then(() => {
-              callback(null, {
-                statusCode: 200,
-                body: JSON.stringify(filesToPush),
+              gh.pushFiles(
+                `FB-JSON storage in CMS on ${Date()}`,
+                filesToPush
+              ).then(() => {
+                callback(null, {
+                  statusCode: 200,
+                  body: JSON.stringify(filesToPush),
+                })
               })
-            })
-          }
-        )
-      })
-      .catch(err => {
-        callback(err)
-      })
-  }
+            }
+          )
+        })
+        .catch(err => {
+          callback(err)
+        })
+    } else {
+      console.log(error)
+    }
+  })
 }
 
 module.exports.handler = sourceFB
