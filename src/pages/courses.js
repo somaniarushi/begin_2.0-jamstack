f94978acc38b2abed0000b650e904b2f1ebe1424
Andrew Kirillov <kirillov.edu@gmail.com>
2020-08-17 04:07:05 -0400

    implemented basic courses page

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -1,11 +1,11 @@
 /** @jsx jsx */
-import { jsx, Card, Heading, Text } from "theme-ui"
-import { useState } from "react"
+import { jsx, Card, Heading, Text, Input, Flex } from "theme-ui"
+import Chip from "../components/chip"
+import { useState, useEffect } from "react"
 import { graphql } from "gatsby"
 import PageTitle from "../components/page_title"
 import Layout from "../components/layout"
 import PropTypes from "prop-types"
-// import _ from "lodash"
 
 export const query = graphql`
   query coursesQuery {
@@ -15,6 +15,8 @@ export const query = graphql`
         number
         title
         description
+        units
+        department
       }
     }
   }
@@ -22,11 +24,61 @@ export const query = graphql`
 
 export default function CoursesPage({ data }) {
   const allCourses = data.allCoursesJson.nodes
-  const [courses] = useState(allCourses)
+  const [courses, setCourses] = useState(allCourses)
+  const [activeTags, setActiveTags] = useState([])
+  const [searchValue, setSearchValue] = useState("")
+
+  const possibleTags = Array.from(
+    new Set(allCourses.map((course) => course.department))
+  )
+
+  function toggleTag(value) {
+    if (activeTags.includes(value)) {
+      setActiveTags(activeTags.filter((filter) => filter !== value))
+    } else {
+      setActiveTags([...activeTags, value])
+    }
+  }
+
+  function filterCourses() {
+    setCourses(
+      allCourses.filter(
+        (course) =>
+          (activeTags.length === 0 || activeTags.includes(course.department)) &&
+          (!searchValue ||
+            course.title.toLowerCase().includes(searchValue.toLowerCase()) ||
+            course.number
+              .toLowerCase()
+              .includes(searchValue.toLocaleLowerCase()))
+      )
+    )
+  }
+
+  useEffect(filterCourses, [activeTags, searchValue])
 
   return (
     <Layout>
       <PageTitle>Courses</PageTitle>
+      <Input
+        placeholder="Search"
+        sx={{ mb: 3 }}
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value)
+        }}
+      />
+      <Flex sx={{ flexWrap: "wrap", mb: 4 }} columns={4}>
+        {possibleTags.map((tag) => (
+          <Chip
+            key={tag}
+            onClick={() => {
+              toggleTag(tag)
+            }}
+          >
+            {tag}
+          </Chip>
+        ))}
+      </Flex>
       {courses.map((course) => (
         <CourseCard key={course.id} course={course} />
       ))}
@@ -46,7 +98,21 @@ function CourseCard({ course }) {
     >
       <Heading>{course.number}</Heading>
       <Heading as="h3">{course.title}</Heading>
-      <Text>{course.description}</Text>
+      <Text
+        sx={{
+          mb: 2,
+        }}
+      >
+        {course.description}
+      </Text>
+      <Text
+        sx={{
+          fontWeight: 800,
+          color: "greyText",
+        }}
+      >
+        {course.units} units
+      </Text>
     </Card>
   )
 }
